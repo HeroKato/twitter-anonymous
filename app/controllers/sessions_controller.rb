b75416bb6f14f0749bf58bf12ec9f94474422343
herokato <h_htb_atm_k@yahoo.co.jp>
2017-11-24 11:31:03 +0000

    modified create and destroy method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,14 @@
 class SessionsController < ApplicationController
    def create
-    
+    auth = request.env["omniauth.auth"]
+    user = User.find_by(provider: auth["provider"], uid: auth["uid"]) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
     flash[:success] ='ログインしました。'
     redirect_to root_path
    end
    
    def destroy
-   
+    session[:user_id] = nil
     flash[:info] = 'ログアウトしました。'
     redirect_to root_path
    end
