0d3759476c5f56a1f884d935396ae8771837bf4f
herokato <h_htb_atm_k@yahoo.co.jp>
2017-11-24 09:39:20 +0000

    modified method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,14 @@
 class User < ActiveRecord::Base
-   #引数に関連するユーザーが存在すればそれを返し、存在しなければ新規に作成する
   class << self
-      def find_or_create_from_auth(auth)
-    #OmniAuthで取得した各データを代入していく
-        provider = auth[:provider]
-        uid = auth[:uid]
-        nickname = auth[:info][:nickname]
-        name = auth[:info][:name]
-        image_url = auth[:info][:image]
-        description = auth[:info][:description]
+      def find_or_create_from_auth_hash(auth)
+        provider = auth_hash[:provider]
+        uid = auth_hash[:uid]
+        nickname = auth_hash[:info][:nickname]
+        image_url = auth_hash[:info][:image]
     
-        User.find_or_create_by(provider: provider, uid: uid) do |user|
+        find_or_create_by(provider: provider, uid: uid) do |user|
           user.nickname = nickname
-          user.name = name
           user.image_url = image_url
-          user.discription = discription
         end
       end
   end
